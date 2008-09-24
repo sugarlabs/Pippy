59ff7a87b63e209307e50ebba540f64dd588dcb6
C. Scott Ananian <cscott@laptop.org>
2008-09-24 15:49:22 -0400

    Add proper license grant to the activity.py file.

--- activity.py
+++ activity.py
@@ -1,3 +1,20 @@
+# Copyright 2007 One Laptop per Child Association, Inc.
+# Written by C. Scott Ananian <cscott@laptop.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""Pippy activity helper classes."""
 from sugar.activity import activity
 
 class ViewSourceActivity(activity.Activity):
@@ -47,6 +64,7 @@ class ViewSourceActivity(activity.Activity):
 
 TARGET_TYPE_TEXT = 80
 class VteActivity(ViewSourceActivity):
+    """Activity subclass built around the Vte terminal widget."""
     def __init__(self, handle):
         import gtk, pango, vte
         from sugar.graphics.toolbutton import ToolButton
