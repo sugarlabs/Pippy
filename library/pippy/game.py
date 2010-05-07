f3a7b17f71990da401917c7647afaad8c33363d6
James Cameron <quozl@laptop.org>
2010-05-07 14:17:57 +1000

    add COPYING file: #1571
    - most of pippy is GPLv2+, but library/pippy/physics is GPLv3+,
      conflict unresolved,
    
    - add missing activity license file COPYING chosen based on
      activity.info license field GPLv2+,
    
    - add missing copyright headers to source files that either didn't
      have them (library/pippy/{__init__,sound}.py) or had abbreviated
      versions (library/pippy/{console,game}.py)
    
    - spell organisation name correctly (s/Per/per),
    
    - express copyright years in full,
    
    - include GPLv3+ LICENSE from Elements upstream SVN, as
      library/pippy/physics/LICENSE
    
    Fixes sugarlabs.org #1571 and laptop.org #9181.

--- library/pippy/game.py
+++ library/pippy/game.py
@@ -1,8 +1,20 @@
 """pygame support for pippy."""
-# Copyright (C) 2007-8 One Laptop Per Child Association, Inc.
-# Licensed under the terms of the GNU GPL v2 or later; see
-# /usr/share/licenses/common-licenses/GPLv2+ for details.
+# Copyright (C) 2007,2008 One Laptop per Child Association, Inc.
 # Written by C. Scott Ananian <cscott@laptop.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 def pause():
     """Display a "Paused" screen and suspend."""
--- pippy_app.py
+++ pippy_app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright 2007 Chris Ball, based on Collabora's "hellomesh" demo.
+# Copyright (C) 2007,2008,2009 Chris Ball, based on Collabora's "hellomesh" demo.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
