ebf2506c5281ef7aff684c93fc89d3eaf4713285
Manuel Kaufmann <humitos@gmail.com>
2012-11-07 09:46:03 -0300

    Add our 'elements' modified .egg to the sys.path
    Signed-off-by: Manuel Kaufmann <humitos@gmail.com>

--- library/pippy/physics/__init__.py
+++ library/pippy/physics/__init__.py
@@ -1,2 +1,8 @@
+import sys
+
 __all__ = ['locals', 'menu']
+
+# Use own 'elements' modified module
+# http://bugs.sugarlabs.org/ticket/3361
+sys.path.append('library/pippy/physics/Elements-0.13-py2.5.egg')
 from elements import Elements
