9ee45b1c912b8eb3726328899259648a934cb548
Rafael Ortiz <dirakx@gmail.com>
2013-03-15 00:18:19 -0500

    import before comment

--- activity.py
+++ activity.py
@@ -16,8 +16,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-"""Pippy activity helper classes."""
 from sugar3.activity import activity
+
+"""Pippy activity helper classes."""
 
 
 class ViewSourceActivity(activity.Activity):
