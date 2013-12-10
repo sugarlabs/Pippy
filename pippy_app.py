a6d076a14cb971ffa2bb6b4d60cf3cf747cf906f
Walter Bender <walter@sugarlabs.org>
2013-12-10 11:35:32 -0500

    Ignacio's patch to fix clipboard support

--- pippy_app.py
+++ pippy_app.py
@@ -418,11 +418,13 @@ Discard changes?')
 
     def __copybutton_cb(self, button):
         global text_buffer
-        text_buffer.copy_clipboard(Gtk.Clipboard())
+        clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+        text_buffer.copy_clipboard(clipboard)
 
     def __pastebutton_cb(self, button):
         global text_buffer
-        text_buffer.paste_clipboard(Gtk.Clipboard(), None, True)
+        clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+        text_buffer.paste_clipboard(clipboard, None, True)
 
     def gobutton_cb(self, button):
         from shutil import copy2
