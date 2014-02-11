4bf657d86e9ca6a91fbcd13beb7d184ae5a7c5e5
Walter Bender <walter@sugarlabs.org>
2014-02-11 13:01:55 -0500

    add copyright

--- filedialog.py
+++ filedialog.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014 Walter Bender
+# Copyright (C) 2014 Ignacio Rodríguez <ignacio@sugarlabs.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 from gi.repository import Gtk, Gdk
 from sugar3.graphics.toolbarbox import ToolbarBox
 from sugar3.graphics.toolbutton import ToolButton
