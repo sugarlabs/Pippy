0aff6496094c10e5b68f40237e1b439a72e100f9
Walter Bender <walter@sugarlabs.org>
2014-12-04 15:33:39 -0500

    corrections suggested by Jean THIERY

--- data/en/tutorials/Tutorial_06_try_except.py
+++ data/en/tutorials/Tutorial_06_try_except.py
@@ -5,5 +5,5 @@ def is_valid_number(num):
     except:
         print "Sorry but, you didn't write a number"
 
-number1 = input("Give me any input and I will tell you if it's a number: ")
+number1 = raw_input("Give me any input and I will tell you if it's a number: ")
 is_valid_number(number1)
