sugarlabs/Pippy

commit b45deb6297ef75dcf1f9d6de09f7815b1a6ca542

Fix issue #27, #29

pippy_app.py
@@ -551,10 +551,13 @@ class PippyActivity(ViewSourceActivity):
         _logger.debug('clicked! %s' % value['path'])
         _file = open(value['path'], 'r')
         lines = _file.readlines()
-        # If there are no pages in the notebook, add one.
-        if self._source_tabs.get_current_page() == -1:
-            self._add_source_cb(None)
         text_buffer = self._source_tabs.get_text_buffer()
+        current_content = text_buffer.get_text(
+            *text_buffer.get_bounds(),
+            include_hidden_chars=True)
+        if current_content != "":
+            # Add a new page to the notebook with the example
+            self._add_source_cb(None)
         if text_buffer.get_modified():
             self._source_tabs.add_tab()
             self.session_data.append(None)

commit 60d73542fbbbb3a4cd3614266bb394adae1bb1f8

Load examples in new tab
With an example already loading, loading another causes empty new tab,
and the previous tab content is erased and loaded with the new example.

Reproducer: start Pippy, load an example, load a second example.

Was caused by commit b45deb6297ef75dcf1f9d6de09f7815b1a6ca542 ('Fix
issue #27, #29') trying to allow multiple examples to be open in
different tabs; after the call to _add_source_cb a new call to
get_text_buffer() was needed.

However, there isn't a good reason to automatically destroy the original
empty tab, and by removing that behaviour the code is simplified.  The
user can always destroy the tab if there is more than one.

pippy_app.py
@@ -562,17 +562,8 @@ class PippyActivity(ViewSourceActivity):
         _logger.debug('clicked! %s' % value['path'])
         _file = open(value['path'], 'r')
         lines = _file.readlines()
+        self._add_source_cb(None)
         text_buffer = self._source_tabs.get_text_buffer()
-        current_content = text_buffer.get_text(
-            *text_buffer.get_bounds(),
-            include_hidden_chars=True)
-        if current_content != "":
-            # Add a new page to the notebook with the example
-            self._add_source_cb(None)
-        if text_buffer.get_modified():
-            self._source_tabs.add_tab()
-            self.session_data.append(None)
-            text_buffer = self._source_tabs.get_text_buffer()
         text_buffer.set_text(''.join(lines))
         text_buffer.set_modified(False)
         self._pippy_instance.metadata['title'] = value['name']