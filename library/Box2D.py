e951a2dcdfdda14a446f0a2c2631a9155e84c749
Ibiam Chihurumnaya <ibiamchihurumnaya@gmail.com>
2020-03-06 23:20:43 +0100

    Remove library as it was never used
    Signed-off-by: Ibiam Chihurumnaya <ibiamchihurumnaya@gmail.com>

--- library/Box2D.py
+++
@@ -1,9 +0,0 @@
-import platform
-
-if platform.machine().startswith('arm'):
-    from box2d_arm7 import *
-else:
-    if platform.architecture()[0] == '64bit':
-        from box2d_64 import *
-    else:
-        from box2d_32 import *
--- library/pippy/physics/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-__all__ = ['locals', 'menu']
-
-# Use own 'elements' modified module
-# http://bugs.sugarlabs.org/ticket/3361
-# sys.path.append('library/pippy/physics/Elements-0.13-py2.5.egg')
-from .myelements import Elements
